327d4dc22d8a5f5db80c4587afafbd15f01147e3
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-01-06 13:19:59 -0500

    Added more functionality in grocery list

--- lib/grocery_list.dart
+++ lib/grocery_list.dart
@@ -7,7 +7,9 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
 
-  String input = "";
+  String item_name = "";
+  String item_qtty = "";
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   List grocery_list = List();
 
   @override
@@ -19,6 +21,7 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             onPressed:(){
@@ -27,17 +30,116 @@ class _GroceryListState extends State<GroceryList> {
                   builder: (BuildContext context){
                     return AlertDialog(
                       title: Text("Enter a new item"),
-                      content: TextField(
-                        onChanged: (String value){
-                          input = value;
-                        },
+                      content: new Container(
+                        width: 260.0,
+                        height: 100.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: const Color(0xFFFFFF),
+                          borderRadius:
+                          new BorderRadius.all(new Radius.circular(32.0)),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            // dialog centre
+                            new Expanded(
+                              child: new Container(
+                                  child: new TextField(
+                                    decoration: new InputDecoration(
+                                      enabledBorder: new OutlineInputBorder(
+                                        borderSide: const BorderSide(color: Colors.deepOrange, width: 1.0),
+                                      ),
+                                      labelStyle: new TextStyle(color: Colors.green),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                                      ),
+                                      filled: true,
+                                      contentPadding: new EdgeInsets.only(
+                                          left: 10.0,
+                                          top: 10.0,
+                                          bottom: 10.0,
+                                          right: 10.0),
+                                      hintText: ' Item name',
+                                      hintStyle: new TextStyle(
+                                        color: Colors.grey.shade500,
+                                        fontSize: 12.0,
+                                        fontFamily: 'helvetica_neue_light',
+                                      ),
+                                    ),
+                                    onChanged: (String value){
+                                      item_name = value;
+                                    },
+                                  )),
+                              flex: 2,
+                            ),
+                            new Expanded(
+                              child: new Container(
+                                  child: new TextField(
+                                    decoration: new InputDecoration(
+                                      enabledBorder: new OutlineInputBorder(
+                                        borderSide: const BorderSide(color: Colors.deepOrange, width: 1.0),
+                                      ),
+                                      labelStyle: new TextStyle(color: Colors.green),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                                      ),
+                                      filled: true,
+                                      contentPadding: new EdgeInsets.only(
+                                          left: 10.0,
+                                          top: 10.0,
+                                          bottom: 10.0,
+                                          right: 10.0),
+                                      hintText: ' Item quantity',
+                                      hintStyle: new TextStyle(
+                                        color: Colors.grey.shade500,
+                                        fontSize: 12.0,
+                                        fontFamily: 'helvetica_neue_light',
+                                      ),
+                                    ),
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (String value){
+                                      item_qtty = value;
+                                    },
+                                  )
+                              ),
+                              flex: 2,
+                            ),
+                          ],
+                        ),
                       ),
+
                       actions: <Widget>[
                         FlatButton(
-                          child: Text("Add"),
+                          padding: new EdgeInsets.all(16.0),
+                          child: new Text(
+                            'Add to list',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontFamily: 'helvetica_neue_light',
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
                           onPressed: (){
                             setState(() {
-                              grocery_list.add(input);
+                              if(item_name != "" && item_qtty == ""){
+                                //Just add the item name, and put quantity as ""
+                                grocery_list.add(item_name);
+                                _displaySnackBarONE(context);
+
+                              }
+                              else if(item_name != "" && item_qtty != ""){
+                                //Add item with its quantity
+                                _displaySnackBarTWO(context);
+                              }
+                              else if(item_name == "" && item_qtty != ""){
+                                //Do nothing bc there's no name, just quantity
+                                //Maybe say nothing added?
+                                _displaySnackBarTHREE(context);
+                              }
+                              item_name = "";
+                              Navigator.of(context, rootNavigator: true).pop();//Close the alert box after adding to list
                             });
                           },
                         )
@@ -60,20 +162,36 @@ class _GroceryListState extends State<GroceryList> {
         )
     );
   }
-}
-
-
-/*
-class GroceryList extends StatelessWidget {
-
-  List list = List();
-
-  @override
-  void initState(){
-    super.initState();
+  _displaySnackBarONE(BuildContext context) {
+    final snackBar = SnackBar(
+        content: Text('Item without quantity added!'),
+        action: SnackBarAction(label: 'Undo',
+            onPressed: (){
+            //Undo the add thing
+            //TO DO
+            }
+            ),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
   }
 
+  _displaySnackBarTWO(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text('Item added!'),
+      action: SnackBarAction(label: 'Undo',
+          onPressed: (){
+            //Undo the add thing
+            //TO DO
+          }
+      ),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
 
+  _displaySnackBarTHREE(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text('Item not added!'),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
 }
-
- */
